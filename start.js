702f04eb020cec6e4dbf8dcd77918b7c154887a1
Nick Rogers <rnrogers@cobaltiron.com>
2018-07-18 22:35:42 -0500

    initial electron

---
+++ start.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, 'build/index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    // mainWindow.loadFile('index.html');
+    // mainWindow.loadFile('build/index.html');
+    mainWindow.loadURL(startUrl);
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.

e491c59d7a14e9b3acc87d81f17c30137bfd07cc
Nick Rogers <rnrogers@cobaltiron.com>
2018-08-04 23:33:04 -0500

    sample ui

--- hadouken/start.js
+++ hadouken/start.js
@@ -10,11 +10,11 @@ const startUrl = process.env.ELECTRON_START_URL || url.format({
 let mainWindow;
 
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, frame: false});
     // mainWindow.loadFile('index.html');
     // mainWindow.loadFile('build/index.html');
     mainWindow.loadURL(startUrl);
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function () {
         mainWindow = null;
